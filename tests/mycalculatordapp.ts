9d1f32ab8ed4a9e9702940425085f066ca43421f
neo <neo@192.168.1.2>
2024-05-16 11:23:59 +0800

    anchor test passing

--- tests/mycalculatordapp.ts
+++ tests/mycalculatordapp.ts
@@ -2,15 +2,80 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Mycalculatordapp } from "../target/types/mycalculatordapp";
 
+const assert = require("assert");
+// const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 describe("mycalculatordapp", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+	const provider = anchor.AnchorProvider.env();
+	anchor.setProvider(provider);
+	const calculator = anchor.web3.Keypair.generate();
+	const program = anchor.workspace.Mycalculatordapp as Program<Mycalculatordapp>;
 
-  const program = anchor.workspace.Mycalculatordapp as Program<Mycalculatordapp>;
+	it("Creates a calculator", async () => {
+		await program.rpc.create("Welcome to Solana", {
+			accounts: {
+				calculator: calculator.publicKey,
+				user: provider.wallet.publicKey,
+				systemProgram: SystemProgram.programId,
+			},
+			signers: [calculator],
+		});
+		const account = await program.account.calculator.fetch(
+			calculator.publicKey
+		);
+		assert.ok(account.greeting === "Welcome to Solana");
+	});
+	it("Adds two numbers", async () => {
+		await program.rpc.add(new anchor.BN(2), new anchor.BN(3), {
+			accounts: {
+				calculator: calculator.publicKey,
+			},
+		});
+		const account = await program.account.calculator.fetch(
+			calculator.publicKey
+		);
+		assert.ok(account.result.eq(new anchor.BN(5)));
+	});
+	// IMPLEMENT YOURSELF! Subtraction test
+	it("Subtracts two numbers", async function () {
+		await program.rpc.subtract(new anchor.BN(32), new anchor.BN(33), {
+			accounts: {
+				calculator: calculator.publicKey,
+			},
+		});
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+		const account = await program.account.calculator.fetch(
+			calculator.publicKey
+		);
+		assert.ok(account.result.eq(new anchor.BN(-1)));
+	});
+
+	// IMPLEMENT YOURSELF! Multiplication test
+	it("Multiplies two numbers", async function () {
+		await program.rpc.multiply(new anchor.BN(2), new anchor.BN(3), {
+			accounts: {
+				calculator: calculator.publicKey,
+			},
+		});
+
+		const account = await program.account.calculator.fetch(
+			calculator.publicKey
+		);
+		assert.ok(account.result.eq(new anchor.BN(6)));
+	});
+
+	// IMPLEMENT YOURSELF! Division test
+	it("Divides two numbers", async function () {
+		await program.rpc.divide(new anchor.BN(10), new anchor.BN(3), {
+			accounts: {
+				calculator: calculator.publicKey,
+			},
+		});
+
+		const account = await program.account.calculator.fetch(
+			calculator.publicKey
+		);
+		assert.ok(account.result.eq(new anchor.BN(3)));
+		assert.ok(account.remainder.eq(new anchor.BN(1)));
+	});
 });
